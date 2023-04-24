18caef6637b10fc585edecee6a0708c3e6213788
Nixa001 <nicolasfaye31@gmail.com>
2023-04-24 10:22:16 +0000

    Style

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
-	"fmt"
 )
 
-type Page struct{
+type Page struct {
 	value string
 }
 
 func main() {
 	http.HandleFunc("/home", homeHandler)
+
+	fmt.Println("Server started  to port 8080")
+	fmt.Println("Open link -> http://localhost:8080/home")
 	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	var templates = template.Must(template.ParseFiles("templates/index.html"))
-	p:=Page{value: "test"}
+	p := Page{value: "test"}
 	err := templates.ExecuteTemplate(w, "index.html", p)
-	fmt.Println("Server started  to port 8080")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
