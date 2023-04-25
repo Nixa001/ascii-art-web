2ac7ec5787b45c59cd68f91cc4ce769e5301f399
Nixa001 <nicolasfaye31@gmail.com>
2023-04-25 17:01:44 +0000

    Feat: Printing inputvalue in Ascii-Art

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,20 +2,13 @@ package handlers
 
 import (
 	"ascii-art-web/asciiart"
-	"fmt"
 	"html/template"
 	"net/http"
 )
 
-// var tpl *template.Template
-
-//	func renderTemplate() {
-//		renderTemplate(w, "home")
-//	}
 func ServerHandler(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseFiles("templates/index.html"))
 	tpl.ExecuteTemplate(w, "index.html", nil)
-	// renderTemplate(w, "home")
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,24 +21,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	input := r.FormValue("inputValue")
 	banner := r.FormValue("banner")
 	Data := asciiart.AsciiArt(input, banner)
-	fmt.Println(Data)
-	fmt.Println("Data")
-	// d := struct {
-	// 	inputValue string
-	// }{
-	// 	inputValue: input,
-	// }
-	tpl.ExecuteTemplate(w, "home.html", nil)
+
+	tpl.Execute(w, map[string]string{"Data": Data})
 
 }
-
-// renderTemplate(w, "home")
-
-// func renderTemplate(w http.ResponseWriter, tmpl string) {
-// 	t, err := template.ParseFiles("./templates/" + tmpl + ".html")
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	t.Execute(w, nil)
-// }
